6fb58f483a5ef05e1a5536fc0a2fcc5fe4537e93
Tyler Chong <tylrchong@gmail.com>
2017-03-05 13:54:39 -1000

    Test Commit

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, redirect
+from flask_socketio import SocketIO
 import os
 from pymongo import MongoClient
 
-# client = MongoClient()
+DB_USER = 'dev'
+DB_PASS = 'python'
+
+client = MongoClient("mongodb://{}:{}@ds050879.mlab.com:50879/peopleseen".format(DB_USER, DB_PASS))
 
 # db = client.test_database
 
 # collection = db.test_collection
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/", methods=['GET'])
 def index():
@@ -17,7 +22,5 @@ def index():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     if port == 5000:
-      app.debug = True
-    app.run(host='0.0.0.0', port=port)
-
-
+      socketio.debug = True
+    socketio.run(app)
