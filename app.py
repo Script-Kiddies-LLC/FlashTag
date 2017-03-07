54284d5aef3da984141411f0ef49f81e37684c93
Tyler Chong <tylrchong@gmail.com>
2017-03-06 19:39:43 -1000

    GG

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
+import os
 from flask import Flask, request, redirect
 from flask_socketio import SocketIO
-import os
 from pymongo import MongoClient
 
-DB_USER = 'dev'
-DB_PASS = 'python'
+DB_USER = "dev"
+DB_PASS = "python"
 
-client = MongoClient("mongodb://{}:{}@ds050879.mlab.com:50879/peopleseen".format(DB_USER, DB_PASS))
+client = MongoClient("mongodb://{}:{}@ds157248.mlab.com:57248/flashtag".format(DB_USER, DB_PASS))
 
-# db = client.test_database
+db = client.test
 
-# collection = db.test_collection
+collection = db.test
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -19,8 +19,50 @@ socketio = SocketIO(app)
 def index():
     return "Hello World"
 
+@socketio.on('connection')
+def connection(message):
+    print "Connect!"
+    valid = false
+    if message.connecting:
+      valid = true
+    data = []
+    emit('connection_response', {
+      'valid': valid,
+      'data': data
+    })
+
+
+@socketio.on('map_get')
+def map_get(message):
+    # From message take in location
+    valid = false
+    if message.location:
+        valid = true
+    # Query DB for places near that location
+    data = []
+    # return all things near
+    emit('map_response', {
+      'valid': valid,
+      'data': data
+    })
+
+
+@socketio.on('marker')
+def connection(message):
+    valid = false
+    # From message take in ID
+    if message.marker:
+      valid = true
+    # Query DB for ID of marker
+    data = []
+    # Return Marker data
+    emit('marker_response', {
+      'valid': valid,
+      'data': data
+    })
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    if port == 5000:
-      socketio.debug = True
+    print "Magic Happening on port: {}".format(port)
     socketio.run(app)
